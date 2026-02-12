a2722326babd00e2074edc4ecd672e2b4ad1c16a
Hector Gomez <hgomezm92@gmail.com>
2026-02-12 19:00:29 +0100

    Implement file moving functionality and update requirements.txt

--- file_sorter.py
+++ file_sorter.py
@@ -1,8 +1,8 @@
-import os
+import os, shutil
 
 PATH = '/home/aituk/Documentos/Prueba'
 
-config: dict[str, list[str]] = {
+CONFIG: dict[str, list[str]] = {
     'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp'],
     'Documents': ['.pdf', '.docx', '.txt', '.xlsx'],
     'Audio': ['.mp3', '.wav', '.aac'],
@@ -11,7 +11,7 @@ config: dict[str, list[str]] = {
     'Scripts': ['.py', '.js', '.sh', '.bat'],
 }
 
-def read_files_list(path: str):
+def read_files_list(path: str) -> dict[str, str]:
     """
     Read directory content and returns a dictionary with file names and 
     extensions skipping directories
@@ -33,7 +33,7 @@ def read_files_list(path: str):
 
     return files
 
-def get_category(extension: str, config: dict[str, list[str]]):
+def get_category(extension: str, config: dict[str, list[str]] = CONFIG) -> str:
     """
     Get category name for a given extension (.xyz) from the config dictionary
     
@@ -50,19 +50,52 @@ def get_category(extension: str, config: dict[str, list[str]]):
 
     return 'Others'
 
-def create_folders(names: dict[str, str], path: str, config: dict[str, list[str]]):
+def create_folders(files: dict[str, str], path: str, config: dict[str, list[str]] = CONFIG) -> None:
     """
     Create needed directories to sort files taking an extension list from a
     dictionary
 
     Args:
-        names: A dictionary with names as keys and extensions with dot as values
+        files: A dictionary with names as keys and extensions with dot as values
         path: Path where directories will be created
         config: A dictionary with categories and extensions that belong to it
     """
-    extensions: set[str] = set(names.values())
+    extensions: set[str] = set(files.values())
     
     categories: set[str] = {get_category(extension, config) for extension in extensions} # set comprehension
     
     for category in categories:
         os.makedirs(os.path.join(path, category), exist_ok=True)
+
+def move_files(files: dict[str, str], path: str, config: dict[str, list[str]] = CONFIG) -> None:
+    """
+    Move files to directories by extension
+
+    Args:
+        files: A dictionary with names as keys and extensions with dot as values
+        path: Original path of files
+        config: A dictionary with categories and extensions that belong to it
+    """
+    print('Moving files...')
+
+    for name, ext in files.items():
+        category: str = get_category(ext, config)
+        if os.path.exists(os.path.join(path, category, name)):
+            print(f'\tSkipped {name} - already exists in {category}')
+            continue
+        print(f'\tMoved {name} to {category}')
+        shutil.move(os.path.join(path, name), os.path.join(path, category, name))
+
+def main(path: str = PATH):
+    print(f'Organizing files in {path}...')
+    files = read_files_list(PATH)
+
+    print('Creating folders...')
+    create_folders(files, PATH, CONFIG)
+
+    move_files(files, PATH, CONFIG)
+
+    print(f'Done! Organized {len(files)} files.')
+
+if __name__ == '__main__':
+    main()
